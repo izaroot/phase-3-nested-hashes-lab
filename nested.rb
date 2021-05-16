6137ebf34d5b637005575fa15da0251d214ee9aa
Isaac <izaroot327@gmail.com>
2021-05-16 17:22:55 -0400

    commit tree ffeb25f588ba156da7b8f2e486408e71c79c066f

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +87,13 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+
+  programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +116,9 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +140,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash
 
 end
